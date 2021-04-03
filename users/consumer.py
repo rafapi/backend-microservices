036c5162a12dbc18400abe8e4086c513990920cd
Rafa <rafa@elementai.com>
2021-04-03 22:35:34 +0100

    Consume the right content per API call

--- users/consumer.py
+++ users/consumer.py
@@ -1,8 +1,10 @@
+import json
+from main import Product, db
 import pika
 
 
 credentials = pika.PlainCredentials('guest', 'guest')
-params = pika.ConnectionParameters(host='rabbitmq', port=5672,virtual_host='/', credentials=credentials)
+params = pika.ConnectionParameters(host='10.0.0.214', port=5672, virtual_host='/', credentials=credentials)
 connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
@@ -12,7 +14,27 @@ channel.queue_declare(queue='main')
 
 def callback(ch, method, properties, body):
     print('Received in main')
-    print(body)
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('Product Created')
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('Product Updated')
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('Product Deleted')
 
 
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
