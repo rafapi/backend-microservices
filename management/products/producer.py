615b72fd7a9216435f04c7bd00c7e17987d3e45c
Rafa <rafa@elementai.com>
2021-04-03 14:35:22 +0100

    Keep track of main queue

--- management/products/producer.py
+++ management/products/producer.py
@@ -7,4 +7,4 @@ channel = connection.channel()
 
 
 def publish():
-    channel.basic_publish(exchange='', routing_key='admin', body='hello')
+    channel.basic_publish(exchange='', routing_key='main', body='hello main')
