6c6d95032e24f077a4bed3f8d74c69e19f7ed4d6
Rafa <rafa@elementai.com>
2021-04-06 20:33:30 +0100

    Declare queue

--- users/producer.py
+++ users/producer.py
@@ -4,10 +4,12 @@ import pika
 
 
 credentials = pika.PlainCredentials('guest', 'guest')
-params = pika.ConnectionParameters(host='10.0.0.214', port=5672, virtual_host='/', credentials=credentials)
+params = pika.ConnectionParameters(host='10.0.0.132', port=5672, virtual_host='/', credentials=credentials)
 connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
+
+channel.queue_declare(queue='admin')
 
 
 def publish(method, body):
