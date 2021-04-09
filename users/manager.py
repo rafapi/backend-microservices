72f93238b0dacf1fe39c65742abf6c6ba070ba48
Rafa <rafa@elementai.com>
2021-04-09 13:22:52 +0100

    Change cammand name to one more appropriate

--- users/manager.py
+++ users/manager.py
@@ -6,7 +6,7 @@ from flask_script import Manager
 migrate = Migrate(app, db_u)
 
 manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+manager.add_command('db_u', MigrateCommand)
 
 
 if __name__ == '__main__':
