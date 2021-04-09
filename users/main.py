61f23e744a127ae8ed9d98febb3eed02dc8db3ee
Rafa <rafa@elementai.com>
2021-04-09 20:29:16 +0100

    Add a default value for the likes field

--- users/main.py
+++ users/main.py
@@ -25,7 +25,7 @@ class Product(db_u.Model):
     id: int = db_u.Column(db_u.Integer, primary_key=True, autoincrement=False)
     title: str = db_u.Column(db_u.String(200))
     image: str = db_u.Column(db_u.String(200))
-    likes: int = db_u.Column(db_u.Integer, autoincrement=False)
+    likes: int = db_u.Column(db_u.Integer, autoincrement=False, default=0)
 
 
 @dataclass
